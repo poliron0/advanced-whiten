9498b8ec02539b3fa0dd3c720e537b00de6ef5dc
sr <sr@rs.com>
2017-11-17 22:14:56 +0200

    Use native whiten code

--- app.ts
+++ app.ts
@@ -1,42 +1,47 @@
+import * as path from 'path';
+import * as fs from 'fs';
 const exec = require('child_process').exec;
-import * as _ from 'lodash'
+import * as _ from 'lodash';
+import whiten from 'whiten'
 
-
-const whitenPromise = (dependenciesString: string) =>
+const whitenPromise = (dependenciesArr: string[]) =>
 	new Promise((resolve, reject) => {
-		exec('cd downloads && whiten ' + dependenciesString, (error, stdout, stderr) => {
-			if (error || stderr) {
-				reject(error + stderr)
-			} else {
-				resolve(stdout)
+		whiten(dependenciesArr, 'npm', (err, tar, cb) => {
+			if (err) {
+				reject(err)
 			}
+
+			let name = dependenciesArr.join(' ').replace(/\//g, '-');
+			name = name ? name : path.basename(process.cwd());
+			tar.pipe(fs.createWriteStream(path.join(process.cwd(), '/downloads' + name + '.tar'))).on('finish', () => {
+				cb();
+				resolve('Done')
+			});
 		})
 	})
 
-const downloadDepString = async (depString: string) => {
-	return whitenPromise(depString)
-}
-
-const downloadDepStringArray = async (depStringArr: string[]) => {
-	for (let str of depStringArr) {
-		console.log('Downloading ' + str)
-		await downloadDepString(str)
+const downloadChunksArray = async (chunksArray: string[][]) => {
+	for(let chunk of chunksArray) {
+		console.log('Downloading ' + chunk.join(' '))
+		await whitenPromise(chunk)
 	}
 }
-
-const toDepStringArray = (dependencies: string[], size = 1) => {
-	let dependenciesChunks = _.chunk(dependencies, size)
-	return dependenciesChunks.map(chunk => chunk.toString().replace(/,/g, ' '))
-	
+const splitToChunks = (dependencies: string[], size = 6) => {
+	return _.chunk(dependencies, size)
 }
+
+const downloadDependencies = (dependencies: string[]) => 
+	downloadChunksArray(splitToChunks(dependencies))
+
+
 let dependencies = Object.keys(require('./json-to-download/package.json').dependencies)
 let devDependencies = Object.keys(require('./json-to-download/package.json').devDependencies)
 
-downloadDepStringArray(toDepStringArray(dependencies))
+downloadDependencies(dependencies)
 	.then(_ => {
 		console.log('Done downloading dependencies')
 	})
-	.then(_ => downloadDepStringArray(toDepStringArray(devDependencies)))
+	.then(_ => downloadDependencies(devDependencies))
 	.then(_ => {
 		console.log('Done downloading dev dependencies')
 	})
